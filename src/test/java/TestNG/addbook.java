fed187ce1a0ed1524a108de5bab332c6debc858b
Varun Manik <varun_kumar-manik@astro.com.my>
2019-03-09 20:26:18 +0800

    made some changes

--- src/test/java/TestNG/addbook.java
+++ src/test/java/TestNG/addbook.java
@@ -22,11 +22,11 @@ public class addbook {
   @Test
   public void verifyaddbook() {
 	  driver.findElement(By.xpath("//div[@class='v-button v-widget']")).click();
-	  driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
+	  driver.findElement(By.id("gwt-uid-5")).sendKeys("Varun");
 	  driver.findElement(By.id("gwt-uid-7")).sendKeys("Selenium");
-	  driver.findElement(By.id("gwt-uid-9")).sendKeys("9876543210");
-	  driver.findElement(By.id("gwt-uid-11")).sendKeys("devops@gmail.com");
-	  driver.findElement(By.id("gwt-uid-13")).sendKeys("06/11/1986");
+	  driver.findElement(By.id("gwt-uid-9")).sendKeys("12345643210");
+	  driver.findElement(By.id("gwt-uid-11")).sendKeys("varun.edureka@gmail.com");
+	  driver.findElement(By.id("gwt-uid-13")).sendKeys("06/01/2019");
 	  driver.findElement(By.xpath("//div[@class='v-button v-widget primary v-button-primary']")).click();
   }
   
